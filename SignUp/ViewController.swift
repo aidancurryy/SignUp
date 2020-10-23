24d5b98349c7105cbcc32c772146ef42c9eb7ea0
aidancurryy <aidancurryy@gmail.com>
2020-10-23 08:55:19 -0400

    Initial Commit

---
+++ SignUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignUp
+//
+//  Created by  on 10/23/20.
+//  Copyright © 2020 ChemsApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
